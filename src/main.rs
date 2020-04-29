3210dd18a0d5525f8d0ecbff5c234c6af15289ae
gatewaynode <44749714+gatewaynode@users.noreply.github.com>
2020-04-29 17:01:08 -0400

    Added multi-field support to editing posts.

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,6 @@ use rustyline::error::ReadlineError;
 use rustyline::Editor;
 
 
-// @TODO Add the verbose mode handlers
 // @TODO Edit should take field types as arguments or open interactive choice of fields to edit
 // @TODO Add post_id checking standard handler (currently just fails if the post_id doesn't exist)
 // @TODO Default mode should be interactive if no args or subcommands are received
@@ -56,6 +55,10 @@ use rustyline::Editor;
 struct State {
     verbose: bool,
     debug: bool,
+    edit_title: bool,
+    edit_body: bool,
+    edit_tags: bool,
+    edit_summary: bool,
     // interactive: bool, // Just thinking out loud
     // connection: SomeConnectionPool,
 }
@@ -65,6 +68,10 @@ impl State {
         State {
             verbose: false,
             debug: false,
+            edit_title: false,
+            edit_body: false,
+            edit_tags: false,
+            edit_summary: false,
         }
     }
 }
@@ -101,6 +108,18 @@ fn main() {
         }
         ("epost", Some(_clone_matches)) => {
             let this_post = _clone_matches.value_of("post_id").unwrap().parse::<i32>().unwrap_or(0);
+            if _clone_matches.is_present("title") {
+                state.edit_title = true;
+            }
+            if _clone_matches.is_present("body") {
+                state.edit_body = true;
+            }
+            if _clone_matches.is_present("tags") {
+                state.edit_tags = true;
+            }
+            if _clone_matches.is_present("summary") {
+                state.edit_summary = true;
+            }
             edit_post(state, this_post)
         }
         ("depost", Some(_clone_matches)) => {
@@ -145,6 +164,26 @@ fn input(prompt: &str) -> String {
     match readline {
         Ok(line) => {
             // editor.add_history_entry(line.as_str());
+            line
+        },
+        Err(ReadlineError::Interrupted) => {
+            exit(0)
+        },
+        Err(ReadlineError::Eof) => {
+            exit(1)
+        }
+        Err(err) => {
+            println!("Error: {:?}", err);
+            exit(1)
+        }
+    }
+}
+
+fn edit(prompt: &str, value: &str) -> String {
+    let mut editor = Editor::<()>::new();
+    let readline = editor.readline_with_initial(prompt, (value, ""));
+    match readline {
+        Ok(line) => {
             line
         },
         Err(ReadlineError::Interrupted) => {
@@ -216,29 +255,62 @@ fn write_post(state: State) {
     }
 }
 
-fn edit_post(state: State, post_id: i32) {
+fn edit_post(mut state: State, post_id: i32) {
     if state.verbose {
         println!("Editing post {}", post_id);
     }
+    if !state.edit_title && !state.edit_body && !state.edit_tags && !state.edit_summary {
+        if state.verbose {
+            println!("No fields selected, assuming body...");
+        }
+        state.edit_body = true;
+    }
 
-    let some_file = NamedTempFile::new();
-    let file_path = String::from(some_file.unwrap().path().to_string_lossy());
+
     let current_content: Post = read_post(post_id);
 
-    fs::write(&file_path, current_content.body)
-        .expect("Something went wrong with writing the temp file");
+    let mut raw_title: String = current_content.title.clone();
+    let mut raw_body: String = current_content.body.clone();
+    let mut raw_tags: String = current_content.tags.clone();
+    let mut raw_summary: String = current_content.summary.clone();
 
-    subprocess::Exec::cmd("vim")
-        .arg(&file_path)
-        .join()
-        .unwrap();
+    if state.edit_title {
+        raw_title = edit("Edit title: ", &raw_title);
+    }
+    if state.edit_body {
+        let some_file = NamedTempFile::new();
+        let file_path = String::from(some_file.unwrap().path().to_string_lossy());
+        fs::write(&file_path, current_content.body)
+            .expect("Something went wrong with writing the temp file");
 
-    let updated_post = Post {
-        body: fs::read_to_string(&file_path).unwrap(),
+        subprocess::Exec::cmd("vim")
+            .arg(&file_path)
+            .join()
+            .unwrap();
+
+        raw_body = fs::read_to_string(&file_path).unwrap();
+        // let updated_post = Post {
+        //     body: fs::read_to_string(&file_path).unwrap(),
+        //     ..current_content
+        // };
+    }
+    if state.edit_tags {
+        raw_tags = edit("Edit tags: ", &raw_tags);
+    }
+    if state.edit_summary {
+        raw_summary = edit("Edit summary: ", &raw_summary);
+    }
+
+    let edited_content = Post {
+        title: raw_title,
+        body: raw_body,
+        tags: raw_tags,
+        summary: raw_summary,
         ..current_content
     };
 
-    let result = update_post(&updated_post).unwrap();
+    // let result = update_post(&updated_post).unwrap();
+    let result = update_post(&edited_content).unwrap();
 
     if state.verbose {
         println!("Update post result: {}", &result);
